ea97cf181a0c86f964e278cf7a3a4fb92f9e9986
acabrera <acabrera@t4xi.com>
2024-04-30 12:50:20 +0200

    [TASK] dummy app health checker

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HealthCheck\HealthCheckGetController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     abort(404);
 });
+
+Route::get('/health-check', HealthCheckGetController::class);
+
