d18ebfe179c564b3d5a6aae98e709f99f1a3262d
acabrera <acabrera@t4xi.com>
2024-05-03 22:58:53 +0200

    [TASK] association doctrine repository implementation

---
+++ src/Administration/Associations/Infrastructure/Persistence/DoctrineAdministrationAssociationRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AnimalSociety\Administration\Associations\Infrastructure\Persistence;
+
+use AnimalSociety\Administration\Associations\Domain\Association;
+use AnimalSociety\Administration\Associations\Domain\AssociationId;
+use AnimalSociety\Administration\Associations\Domain\AssociationRepository;
+use AnimalSociety\Shared\Domain\Criteria\Criteria;
+use AnimalSociety\Shared\Infrastructure\Database\Doctrine\DoctrineCriteriaConverter;
+use AnimalSociety\Shared\Infrastructure\Database\Doctrine\DoctrineRepository;
+
+final class DoctrineAdministrationAssociationRepository extends DoctrineRepository implements AssociationRepository
+{
+    public function save(Association $association): void
+    {
+        $this->persist($association);
+    }
+
+    public function find(AssociationId $id): ?Association
+    {
+        return $this->repository(Association::class)->find($id);
+    }
+
+    /**
+     * @return Association[]
+     */
+    public function findAll(): array
+    {
+        return $this->repository(Association::class)->findAll();
+    }
+
+    /**
+     * @return Association[]
+     */
+    public function matching(Criteria $criteria): array
+    {
+        $doctrineCriteria = DoctrineCriteriaConverter::convert($criteria);
+
+        return $this->repository(Association::class)->matching($doctrineCriteria)->toArray();
+    }
+}
