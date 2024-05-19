4af857a5eb6f985a725426c90bfaf5712c70ea18
acabrera <acabrera@t4xi.com>
2024-05-19 09:45:05 +0200

    [TASK] remove unused providers

--- config/app.php
+++ config/app.php
@@ -165,9 +165,6 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         LaravelDoctrine\ORM\DoctrineServiceProvider::class,
         LaravelDoctrine\Extensions\GedmoExtensionsServiceProvider::class,
