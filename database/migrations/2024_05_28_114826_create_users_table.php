dc28897d395999553c9e1b9a20ad5db37f7ff327
acabrera <acabrera@t4xi.com>
2024-05-28 23:16:40 +0200

    [TASK] database migrations

---
+++ database/migrations/2024_05_28_114826_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('first_last_name');
+            $table->string('second_last_name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('association_id');
+            $table->string('role');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
