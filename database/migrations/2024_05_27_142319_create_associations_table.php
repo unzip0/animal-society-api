2c3e9db65df1eb8aaa8e0dee57806dcbaeb0e13b
acabrera <acabrera@t4xi.com>
2024-05-28 23:16:40 +0200

    [TASK] database migrations

---
+++ database/migrations/2024_05_27_142319_create_associations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('associations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('cif');
+            $table->integer('city_id');
+            $table->string('email');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('associations');
+    }
+};
