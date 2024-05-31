c6bd161f8c18ecb786e00e86679053ed972a68ef
acabrera <acabrera@t4xi.com>
2024-05-31 23:01:11 +0200

    [TASK] animal species domain

---
+++ src/Administration/Animals/AnimalsSpecies/Domain/AnimalSpeciesId.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AnimalSociety\Administration\Animals\AnimalsSpecies\Domain;
+
+use AnimalSociety\Shared\Domain\ValueObject\Uuid;
+
+final class AnimalSpeciesId extends Uuid {}
