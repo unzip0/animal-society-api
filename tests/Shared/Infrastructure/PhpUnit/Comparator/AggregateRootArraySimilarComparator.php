a78b720e2f18948cb729dd3d51adb3d801a5e3f0
acabrera <acabrera@t4xi.com>
2024-05-31 12:26:26 +0200

    [TASK] password skip for test comparators

--- tests/Shared/Infrastructure/PhpUnit/Comparator/AggregateRootArraySimilarComparator.php
+++ tests/Shared/Infrastructure/PhpUnit/Comparator/AggregateRootArraySimilarComparator.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace AnimalSociety\Tests\Shared\Infrastructure\PhpUnit\Comparator;
 
 use AnimalSociety\Shared\Domain\Aggregate\AggregateRoot;
-use AnimalSociety\Shared\Infrastructure\Test\Shared\Domain\TestUtils;
+use AnimalSociety\Tests\Shared\Domain\TestUtils;
 use SebastianBergmann\Comparator\Comparator;
 use SebastianBergmann\Comparator\ComparisonFailure;
 
+use SebastianBergmann\Exporter\Exporter;
 use function Lambdish\Phunctional\all;
 use function Lambdish\Phunctional\any;
 use function Lambdish\Phunctional\instance_of;
@@ -25,14 +26,14 @@ final class AggregateRootArraySimilarComparator extends Comparator
 
     public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false): void
     {
+        $exporter = new Exporter();
+
         if (!$this->contains($expected, $actual) || count($expected) !== count($actual)) {
             throw new ComparisonFailure(
                 $expected,
                 $actual,
-                // $this->exporter->export($expected),
-                // $this->exporter->export($actual),
-                (string) $expected,
-                (string) $actual,
+                $exporter->export($expected),
+                $exporter->export($actual),
                 'Failed asserting the collection of AGs contains all the expected elements.'
             );
         }
