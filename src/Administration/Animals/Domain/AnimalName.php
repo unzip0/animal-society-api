c9abdc77bc3eea38d6fb7ea636fbf647dc7afd8d
acabrera <acabrera@t4xi.com>
2024-05-31 23:01:27 +0200

    [TASK] animal domain

---
+++ src/Administration/Animals/Domain/AnimalName.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AnimalSociety\Administration\Animals\Domain;
+
+use AnimalSociety\Shared\Domain\ValueObject\StringValueObject;
+
+final class AnimalName extends StringValueObject {}
