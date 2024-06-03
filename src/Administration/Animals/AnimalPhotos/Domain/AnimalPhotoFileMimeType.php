ccb77c156939bdc98e62c17edfc7af7dd545b7ce
acabrera <acabrera@t4xi.com>
2024-06-03 16:06:12 +0200

    [TASK] fix exceptions throw

--- src/Administration/Animals/AnimalPhotos/Domain/AnimalPhotoFileMimeType.php
+++ src/Administration/Animals/AnimalPhotos/Domain/AnimalPhotoFileMimeType.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace AnimalSociety\Administration\Animals\AnimalPhotos\Domain;
 
-use AnimalSociety\Administration\Animals\AnimalPhotos\Domain\Exceptions\InvalidFileNameException;
+use AnimalSociety\Administration\Animals\AnimalPhotos\Domain\Exceptions\InvalidFileMimeTypeException;
 use AnimalSociety\Shared\Domain\ValueObject\StringValueObject;
 
 final class AnimalPhotoFileMimeType extends StringValueObject
 {
-    private const string MIMETYPE_PNG = 'png';
-    private const string MIMETYPE_JPG = 'jpg';
+    private const string MIMETYPE_PNG = 'image/png';
+    private const string MIMETYPE_JPG = 'image/jpg';
+    private const string MIMETYPE_JPEG = 'image/jpeg';
     private const array ALLOWED_MIMETYPES = [
         self::MIMETYPE_PNG,
         self::MIMETYPE_JPG,
+        self::MIMETYPE_JPEG,
     ];
 
     public function __construct(string $mimetype)
@@ -25,7 +27,7 @@ final class AnimalPhotoFileMimeType extends StringValueObject
     private function ensureIsValidFileMimeType(string $mimetype): void
     {
         if (!in_array($mimetype, self::ALLOWED_MIMETYPES, true)) {
-            throw InvalidFileNameException::create();
+            throw InvalidFileMimeTypeException::create();
         }
     }
 }
