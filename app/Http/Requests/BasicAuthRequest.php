029d3dbbba4f0a7e14548f02fd5e4f686f0830f3
acabrera <acabrera@t4xi.com>
2024-06-04 15:46:12 +0200

    [TASK] basic auth request

---
+++ app/Http/Requests/BasicAuthRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+class BasicAuthRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'association_id' => 'string|required',
+        ];
+    }
+
+    public function getAssociationId(): string
+    {
+        return (string) $this->input('association_id');
+    }
+}
